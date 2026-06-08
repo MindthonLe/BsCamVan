341c333485d5f5edbce420c9e0b7f55b45d2923a
MindthonLe <leminhtri.hmcl@gmail.com>
2026-06-08 17:12:52 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig(() => {
   return {
     Base: '/BsCamVan/',
     plugins: [react(), tailwindcss()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env..GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
